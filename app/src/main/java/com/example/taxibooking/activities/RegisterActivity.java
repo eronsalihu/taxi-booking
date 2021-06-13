7bd370b245448c76d00402a87efad08766d1e554
erisera19 <58450146+erisera19@users.noreply.github.com>
2021-06-13 20:06:53 +0200

    modifying the register activity for the user

--- app/src/main/java/com/example/taxibooking/activities/RegisterActivity.java
+++ app/src/main/java/com/example/taxibooking/activities/RegisterActivity.java
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.taxiBooking.R;
 import com.example.taxibooking.models.User;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -21,16 +22,15 @@ import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText registerEmail;
-    EditText registerPassword;
-    EditText fullName;
-    EditText confirmPassword;
-    CheckBox termsAndCondition;
-    Button signUpBtn;
-    LinearLayout alreadyHaveAnAccount;
-    FirebaseDatabase database;
-    DatabaseReference databaseReference;
-    FirebaseAuth mAuth;
+     EditText registerEmail;
+     EditText registerPassword;
+     EditText fullName;
+     EditText confirmPassword;
+     CheckBox termsAndCondition;
+     Button signUpBtn;
+     LinearLayout alreadyHaveAnAccount;
+     FirebaseAuth mAuth;
+     DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,20 +61,17 @@ public class RegisterActivity extends AppCompatActivity {
         String password = registerPassword.getText().toString();
         String conPassword = confirmPassword.getText().toString();
 
-        database = FirebaseDatabase.getInstance();
-        databaseReference = database.getReference("User");
         mAuth = FirebaseAuth.getInstance();
-
-
-        User user = new User(name,email,password,null,null,null);
-        String keyID = databaseReference.push().getKey();
-        databaseReference.child(keyID).setValue(user);
-
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        User user = new User(name,email,"tag","tag","tag");
         if (validate(name, email, password, conPassword)) {
 
             FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
                         if (task.isSuccessful()) {
+                            String currentUserID = mAuth.getCurrentUser().getUid();
+                            databaseReference.child("User").child(currentUserID).setValue(user);
                             Toast.makeText(this, "Account created successfully!", Toast.LENGTH_SHORT).show();
+
                             Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).sendEmailVerification().addOnCompleteListener(task1 -> {
                                 if (task1.isSuccessful()) {
                                     Toast.makeText(this, "Verify your email to continue!", Toast.LENGTH_SHORT).show();
@@ -99,7 +96,6 @@ public class RegisterActivity extends AppCompatActivity {
 
 
     }
-
     private boolean validate(String name, String email, String password, String conPassword) {
         if (name.isEmpty()) {
             fullName.setError("Enter your Name");
--- app/src/main/java/com/example/taxibooking/models/User.java
+++ app/src/main/java/com/example/taxibooking/models/User.java
@@ -8,44 +8,63 @@ public class User {
     private String gender;
     private String dateOfBirth;
 
-    public User() {
-    }
+    public User(){}
 
     public User(String fullName, String email,
-                String password, String phone, String gender,
+                String phone, String gender,
                 String dateOfBirth) {
         this.fullName = fullName;
         this.email = email;
-        this.password = password;
         this.phone = phone;
         this.gender = gender;
         this.dateOfBirth = dateOfBirth;
     }
 
-    public String getFirstName() {
+    public String getFullName() {
         return fullName;
     }
 
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getPhone() {
         return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
     public String getGender() {
         return gender;
     }
 
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
     public String getDateOfBirth() {
         return dateOfBirth;
     }
 
-
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
